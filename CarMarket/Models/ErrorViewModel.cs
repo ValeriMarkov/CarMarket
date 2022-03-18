5bda7727985f6633a63d3f8bb887d1d33f73c346
Valeri <v.markow.97@gmail.com>
2022-03-18 15:07:33 +0200

    Add project files.

---
+++ CarMarket/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CarMarket.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
