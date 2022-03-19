00ff14a06737b446d18cee67fd4507bb8c0f004d
Valeri <v.markow.97@gmail.com>
2022-03-19 14:25:04 +0200

    update

---
+++ CarMarket/Data/Enums/Features.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarMarket.Data.Enums
+{
+    public enum Features
+    {
+        Airbags = 1,
+        BackAirBags,
+        DualAirBags,
+        ABS,
+        EPS,
+        ISOFIXAnchorages,
+        RearDoorChildLock,
+        DayAndNightIRVM,
+        AdjustableWingMirrors,
+        AC,
+        PowerSteering,
+        PowerWindows,
+        MusicSystem,
+        Radio,
+        DoorLocking,
+        Immobiliser,
+        Alarm,
+        Socket12V,
+        Sunroof,
+        LeatherSeats,
+        HeatedSeats,
+        BackupCamera,
+        Parktronic,
+        NavigationSystem,
+        Bluetooth,
+        RemoteStart,
+        StartStop,
+        FourWheelDrive,
+        Heater,
+        LeftWheel,
+        RightWhel,
+    }
+}
---
+++ CarMarket/Models/Car.cs
@@ -0,0 +1,28 @@
+﻿using CarMarket.Data;
+using CarMarket.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarMarket.Models
+{
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Picture { get; set; }
+        public Brand Brand { get; set; }
+        public string Model { get; set; }
+        public double Engine { get; set; }
+        public int HorsePower { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? ManufactureDate { get; set; }
+        public int TotalMileage { get; set; }
+        public string Description { get; set; }
+        public double SalePrice { get; set; }
+
+        public List<Features> Features { get; set; }
+    }
+}
